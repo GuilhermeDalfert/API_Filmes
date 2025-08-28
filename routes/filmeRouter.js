a31f7415eba043bf9bccd9c9c8f0b8ec125d6295
Guilherme Azambuja <gdazambuja777@gmail.com>
2025-08-28 16:48:51 -0300

    API_Filmes v0.3: classes e endpoints Filme criados e testados!

--- routes/filmeRouter.js
+++ routes/filmeRouter.js
@@ -4,7 +4,8 @@ const filmeController = require('../controllers/filmeController');
 
 router.get('/filmes', filmeController.listaFilmes);
 router.get('/filmes/:id', filmeController.lerFilme);
-
+router.put('/filmes/:id', filmeController.atualizaFilme);
 router.post('/filmes', filmeController.criaFilme);
+router.delete('/filmes/:id', filmeController.deletaFilme);
 
 module.exports = router;
